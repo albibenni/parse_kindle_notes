2c4a550789b54e55b8ac2646b547175061f32aa1
Alberto Benatti <albi.benni8@gmail.com>
2025-03-03 15:58:07 +0100

    init arg pars

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,11 @@
 mod handle_kindle_notes;
 fn main() {
     println!("Hello, world!");
-    let path = String::from("test");
+
+    let arg: String = std::env::args()
+        .nth(1)
+        .expect("provide a file name as argument");
+    let mut path: String = String::from("~/").to_owned();
+    path.push_str(&arg);
     handle_kindle_notes::parse_kindle_notes(path);
 }
