468a9447021c47ba75733c983a0af8b8b5a96a27
Alberto Benatti <albi.benni8@gmail.com>
2025-03-04 11:25:51 +0100

    1st test in rust and parsing string in rust

--- src/handle_kindle_notes.rs
+++ src/handle_kindle_notes.rs
@@ -1,3 +1,33 @@
-pub fn parse_kindle_notes(path: String) {
+pub fn parse_kindle_notes(path: &str) {
     println!("{}", path);
+}
+
+/// Checks if a given file name ends with .txt extension
+///
+/// # Arguments
+///
+/// * `file_name` - The name of the file to check
+///
+/// # Returns
+///
+/// `true` if the file name ends with ".txt", otherwise `false`
+pub fn check_file_name(file_name: &str, extension_supported: &str) -> bool {
+    let ends_with: bool = file_name.ends_with(extension_supported);
+    return ends_with && file_name.len() > extension_supported.len();
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_check_file_name() {
+        let extension_supported = ".txt";
+        assert!(check_file_name("notes.txt", extension_supported));
+        assert!(check_file_name("My Kindle Notes.txt", extension_supported));
+        assert!(!check_file_name("notes.pdf", extension_supported));
+        assert!(!check_file_name("notes.txt.pdf", extension_supported));
+        assert!(!check_file_name("notes", extension_supported));
+        assert!(!check_file_name(".txt", extension_supported));
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,37 @@
 #[path = "./handle_kindle_notes.rs"]
 mod handle_kindle_notes;
+
 fn main() {
     println!("Hello, world!");
 
-    let arg: String = std::env::args()
-        .nth(1)
-        .expect("provide a file name as argument");
-    let mut path: String = String::from("~/").to_owned();
-    path.push_str(&arg);
-    handle_kindle_notes::parse_kindle_notes(path);
+    let mut args = std::env::args();
+    // .nth(1)
+    // .expect("provide a file name as argument");
+
+    let extension_supported = ".txt";
+    match args.len() {
+        1 => {
+            println!("provide a file name as argument");
+            return;
+        }
+        2 => {
+            let s = args.nth(1).expect("provide a file name as argument");
+            handle_kindle_notes::check_file_name(&s, extension_supported);
+            handle_kindle_notes::parse_kindle_notes(&s);
+        }
+        3 => {
+            //let cmd = args.nth(1).expect("provide a cmd")
+            let res: Vec<String> = args.into_iter().collect();
+            println!("{:?}", res);
+            //handle_kindle_notes::parse_kindle_notes(path);
+        }
+        _ => {
+            println!("provide a file name as argument");
+            return;
+        }
+    }
+
+    // let mut path: String = String::from("~/").to_owned();
+    // path.push_str(&args.nth(1).unwrap());
+    // handle_kindle_notes::parse_kindle_notes(path);
 }
